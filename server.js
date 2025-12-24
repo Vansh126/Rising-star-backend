9ef09d7327af186535235acdc2fa59d93b2929bc
Vansh126 <vsvanshsingh814@gmail.com>
2025-12-24 12:51:40 +0530

    routes aded

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose
         process.exit(1);
     });
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://new-rising-star-badminton-academy.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
